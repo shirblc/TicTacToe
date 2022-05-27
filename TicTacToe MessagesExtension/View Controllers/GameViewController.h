22c597b9f5c7e468d145887b6035216c9569b702
Shir Bar Lev <shirblc@icloud.com>
2022-05-27 07:12:17 +0100

    Added initial Game area VC

---
+++ TicTacToe MessagesExtension/View Controllers/GameViewController.h
@@ -0,0 +1,16 @@
+//
+//  GameViewController.h
+//  TicTacToe MessagesExtension
+//
+//  Created by Shir Bar Lev on 27/05/2022.
+//
+
+#import <Messages/Messages.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GameViewController : MSMessagesAppViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TicTacToe MessagesExtension/View Controllers/GameViewController.m
@@ -0,0 +1,22 @@
+//
+//  GameViewController.m
+//  TicTacToe MessagesExtension
+//
+//  Created by Shir Bar Lev on 27/05/2022.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
